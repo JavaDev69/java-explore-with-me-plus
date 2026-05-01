38b3c959685af16a7573601bf7702cd17066741e
Alexander Scatov <scatovalexander@yandex.ru>
2026-05-01 15:02:44 +0300

    исправлены ошибки импортов

--- ewm-main-service/main-service/src/main/java/ru/practicum/compilation/controller/AdminCompilationController.java
+++ ewm-main-service/main-service/src/main/java/ru/practicum/compilation/controller/AdminCompilationController.java
@@ -6,10 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.compilation.CompilationDto;
+import ru.practicum.compilation.NewCompilationDto;
 import ru.practicum.compilation.UpdateCompilationRequest;
 import ru.practicum.compilation.service.CompilationService;
 
-import ru.practicum.dto.compilation.NewCompilationDto;
 
 
 @RestController
