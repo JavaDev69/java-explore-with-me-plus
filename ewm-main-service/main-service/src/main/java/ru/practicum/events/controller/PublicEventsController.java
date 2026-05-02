e4e2682fe19631ac4bd268552a24a8e0f14d6a13
Alexander Scatov <scatovalexander@yandex.ru>
2026-05-02 21:49:58 +0300

    добавлена валидация получения событий с фильтрацией

--- ewm-main-service/main-service/src/main/java/ru/practicum/events/controller/PublicEventsController.java
+++ ewm-main-service/main-service/src/main/java/ru/practicum/events/controller/PublicEventsController.java
@@ -1,6 +1,7 @@
 package ru.practicum.events.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -24,18 +25,40 @@ public class PublicEventsController {
 
     @GetMapping
     public ResponseEntity<List<EventShortDto>> getEvents(
-            @RequestParam(required = false) String text,
-            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false)
+            @Size(max = 1000, message = "Text length must be less than or equal to 1000 characters")
+            String text,
+
+            @RequestParam(required = false)
+            List<@Positive(message = "Category IDs must be positive numbers") Long> categories,
+
             @RequestParam(required = false) Boolean paid,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime rangeStart,
+
+            @RequestParam(required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime rangeEnd,
+
             @RequestParam(defaultValue = "false") Boolean onlyAvailable,
-            @RequestParam(defaultValue = "EVENT_DATE") String sort,
-            @RequestParam(defaultValue = "0") Integer from,
-            @RequestParam(defaultValue = "10") Integer size,
+
+            @RequestParam(defaultValue = "EVENT_DATE")
+            @Pattern(regexp = "EVENT_DATE|VIEWS", message = "Sort must be either 'EVENT_DATE' or 'VIEWS'")
+            String sort,
+
+            @RequestParam(defaultValue = "0")
+            @Min(value = 0, message = "From must be greater than or equal to 0")
+            Integer from,
+
+            @RequestParam(defaultValue = "10")
+            @Min(value = 1, message = "Size must be greater than 0")
+            @Max(value = 1000, message = "Size must be less than or equal to 1000")
+            Integer size,
+
             HttpServletRequest request
     ) {
-
         EndpointHit hit = EndpointHit.builder()
                 .app("ewm-main-service")
                 .uri(request.getRequestURI())
