10f8968493d365b8e264cd007fe0994768649885
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-05-08 08:57:26 +0300

    fix Checkstyle

--- ewm-main-service/main-dto/src/main/java/ru/practicum/comments/NewCommentDto.java
+++ ewm-main-service/main-dto/src/main/java/ru/practicum/comments/NewCommentDto.java
@@ -7,8 +7,9 @@ import lombok.Data;
 
 @Data
 public class NewCommentDto {
+
     @NotBlank(message = "Comment text cannot be blank")
-    @NotNull(message="Comment cannot be null")
+    @NotNull(message = "Comment cannot be null")
     @Size(max = 1000, message = "Comment text cannot exceed 1000 characters")
     private String text;
 }
